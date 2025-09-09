b8d75aaf3bfe5cae0b763c115dbee59de8988cee
Ebrahim Elnemr <113116449+ebrahim969@users.noreply.github.com>
2025-09-09 17:57:17 +0300

    fix: Update MAPS_API_KEY declaration and improve syntax for resource value assignment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
-def MAPS_API_KEY = project.hasProperty("MAPS_API_KEY") ? project.MAPS_API_KEY : ""
+
+
+val MAPS_API_KEY: String = project.findProperty("MAPS_API_KEY") as String? ?: ""
 
 android {
     namespace = "com.example.postify"
@@ -31,7 +33,7 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
-        resValue "string", "google_maps_key", MAPS_API_KEY
+        resValue("string", "google_maps_key", MAPS_API_KEY)
     }
 
     buildTypes {
